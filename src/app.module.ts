44ddd7153d2b040bff8c5d19e8414ad1ab9c168f
Phạm Quốc Vỹ <phamquocvy@Phams-Mac-mini.local>
2023-12-23 22:28:57 +0700

    Config connect DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModuleModule } from './user-module/user-module.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'database/data-source';
 @Module({
-  imports: [UserModuleModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
